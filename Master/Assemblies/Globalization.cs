689b4786b5ef1da0ad94194227350fb53228275f
Bobo <Bobo@BOBO-DEV>
2022-11-22 21:04:02 +0100

    good fixes

--- Master/Assemblies/Globalization.cs
+++ Master/Assemblies/Globalization.cs
@@ -2,17 +2,18 @@ using System.Globalization;
 
 public partial class Globalization
 {
-	private const string TableDefinition = "Name NVARCHAR(10), NativeName NVARCHAR(100), DisplayName NVARCHAR(100), EnglishName NVARCHAR(100), ThreeLetterISO NVARCHAR(3), TwoLetterISO NVARCHAR(2), IsNeutralCulture BIT, LCID INT, ANSICodePage INT";
+	private const string TableDefinition = "CultureName NVARCHAR(15), NativeName NVARCHAR(100), DisplayName NVARCHAR(100), EnglishName NVARCHAR(100), ThreeLetterISO NVARCHAR(5), TwoLetterISO NVARCHAR(5), IsNeutralCulture BIT, LCID INT, ANSICodePage INT";
 
-	private static void FillGetCultureRows(object objCultureData, out string name, out string nativeName,
-		out string displayName, out string englishName, out string threeLetterISO, out string twoLetterISO,
-		out bool isNeutralCulture, out int lcid, out int ansiCodePage)
+	private static void FillGetCultureRows(object objCultureData, out string cultureName,
+		out string nativeName, out string displayName, out string englishName,
+		out string threeLetterISO, out string twoLetterISO, out bool isNeutralCulture,
+		out int lcid, out int ansiCodePage)
 	{
 		CultureData cultureData = objCultureData as CultureData;
-		name = cultureData.Name;
+		cultureName = cultureData.TextInfo.CultureName;
 		nativeName = cultureData.NativeName;
 		displayName = cultureData.DisplayName;
-		englishName = cultureData.EnglishName;
+		englishName = cultureData.EnglishName;		
 		threeLetterISO = cultureData.ThreeLetterISOLanguageName;
 		twoLetterISO = cultureData.TwoLetterISOLanguageName;
 		isNeutralCulture = cultureData.IsNeutralCulture;
